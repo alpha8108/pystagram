12ded7d649f6dfc3022ccacddf2e7aa43c7dd9af
JeongGeun <alpha8108@naver.com>
2024-03-28 16:44:35 +0900

    관리자 페이지에서 썸네일 이미지 표시방법1 직접 admin을 조작해서 썸네일 표시코드 추가(입문자용x)

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,6 @@
+from django.contrib.admin.widgets import AdminFileWidget
+from django.db import models
+from django.utils.safestring import mark_safe
 from django.contrib import admin
 from posts.models import Post, PostImage, Comment
 
@@ -7,9 +10,24 @@ class CommentInline(admin.TabularInline):
     model = Comment
     extra = 1
 
+# AdminFileWidget은 관리자 페이지에서 '파일 선택' 버튼을 보여주는 부분이다.
+# 이 Widget을 커스텀하여, <img> 태그를 추가한다 
+class InlineImageWidget(AdminFileWidget):
+    def render(self, name, value, attrs=None, renderer=None):
+        html = super().render(name, value, attrs, renderer)
+        if value and getattr(value, 'url', None):
+            html = mark_safe(f'<img src="{value.url}" height=150>') + html
+        return html
+
+
 class PostImageInline(admin.TabularInline):
     model = PostImage
     extra = 1
+    formfield_overrides = {
+        models.ImageField: {
+            "widget": InlineImageWidget,
+        }
+    }
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
