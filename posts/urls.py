9d6141725853813677f50b95f154aaa95a97e4fb
JeongGeun <alpha8108@naver.com>
2024-03-28 22:44:46 +0900

    댓글 작성처리 및 댓글 수 표시

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from posts.views import*
 
 urlpatterns = [
     path('feeds/', feeds),
+    path('comment_add/', comment_add),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect 
+from django.views.decorators.http import require_POST
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect 
 from posts.models import Post
 from posts.forms import CommentForm
 
@@ -17,3 +19,30 @@ def feeds(request):
         'comment_form': comment_form,
                 }
     return render(request, 'posts/feeds.html', context)
+
+@require_POST #댓글 작성을 처리할 View, Post 요청만 허용한다 
+def comment_add(request):
+    # request.POST로 전달된 데이터를 사용해 CommentForm 인스턴스를 생성
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # commit=False 옵션으로 메모리상에 Comment 객체 생성
+        comment = form.save(commit=False)
+
+        # Comment 생성에 필요한 사용자 정보를 request에서 가져와 할당
+        comment.user = request.user
+
+        #DB에 Comment객체 저장
+        comment.save()
+
+        #생성된 Comment의 정보 확인
+        print(comment.id)
+        print(comment.content)
+        print(comment.user)
+        #생성 완료 후에는 피드 페이지로 다시 이동
+        #return redirect("/posts/feeds/")
+        # 생성한 comment에서 연결된 post정보를 가져와서 id 값을 사용 / 
+        # 댓글 작성이 완료되고 피드페이지에서 스크롤될 위치를 지정할 수 있으려면
+        # redirect함수를 쓸 수 없기에 대신 HttpResponseRedirect 객체를 사용하게 된 것.
+        # redirect 함수에서는 URL뒤에 추가 문자열을 붗이는 것을 허용하지 않으므로 
+        # redirect시킬 URL뒤에 #post-2 와 같은 문자열을 추가하려면 HttpResponseRedirect 객체를 직접 사용한것 
+        return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}") 
