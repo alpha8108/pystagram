a1449e13ab0a02e3a6d42688c8add5a95569e205
JeongGeun <alpha8108@naver.com>
2024-03-29 00:24:00 +0900

    게시글 작성 구현 완성 및 여러 이미지 업로드 for문 참고

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.views.decorators.http import require_POST
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponseForbidden
-from posts.models import Post, Comment
+from posts.models import Post, Comment, PostImage
 from posts.forms import CommentForm, PostForm
 
 # Create your views here.
@@ -57,6 +57,30 @@ def comment_delete(request, comment_id):
             return HttpResponseForbidden('이 댓글을 삭제할 권한이 없습니다.')
     
 def post_add(request):
-     form = PostForm()
-     context = {"form": form}
-     return render(request, 'posts/post_add.html', context)
+    if request.method == "POST":
+          #request.POST로 온 데이터("content")는 PostForm으로 처리
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            #Post의 "user" 값은 request에서 가져와 자동 할당한다
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+
+            #Post를 생성한 후 
+            #request.FILES.getlist("images")로 전송된 이미지들을 순회하며 PostImage 객체를 생성한다
+            for image_file in request.FILES.getlist("images"): # "images"는 전달된 input의 'name'속성
+                # request.FILES 또는 request.FILES.getlist()로 가져온 파일은
+                # Model의 ImageField 부분에 곧바로 할당한다 
+                PostImage.objects.create(post=post, photo=image_file)
+
+                # 모든 PostImage의 Post의 생성이 완료되면
+                # 피드 페이지로 이동하며 생성된 Post의 위치로 스크롤되도록 한다
+            url = f'/posts/feeds/#post-{post.id}'
+            return HttpResponseRedirect(url)
+        #GET요청일 때는 빈 form을 보여주도록 한다.
+    else:
+        form = PostForm()
+
+        context = {"form": form}
+        return render(request, 'posts/post_add.html', context)
