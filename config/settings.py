8af7194c41f229226696b43ff858f3fe653780ec
JeongGeun <alpha8108@naver.com>
2024-03-28 10:38:57 +0900

    커스텀유저 모델 생성 후 마이그레이션

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+# 사용법:{App 이름}.{Model 이름}
+AUTH_USER_MODEL = "users.User"
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR / 'templates' # 템플릿 경로 할당 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'users',
 ]
 
 MIDDLEWARE = [
