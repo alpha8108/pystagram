6445e24b038aade3c899e5642a8b6bc30fe5dd4a
JeongGeun <alpha8108@naver.com>
2024-03-28 16:36:54 +0900

    admin에 연관 객체 표시

--- posts/admin.py
+++ posts/admin.py
@@ -3,12 +3,23 @@ from posts.models import Post, PostImage, Comment
 
 # Register your models here.
 
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         'id',
         'content',
+    ]
+    inlines = [
+        CommentInline,
+        PostImageInline,
     ]
 
 @admin.register(PostImage)
