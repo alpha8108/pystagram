2d786d528066c23bdf17a6d1c90d263c38261fd6
JeongGeun <alpha8108@naver.com>
2024-04-11 18:50:28 +0900

    팔로우기능 구현 따라했지만 에러남 귀찮으니.. 마무리

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,8 @@ urlpatterns= [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('signup/', signup, name='signup'),
+    path('<int:user_id>/profile/', profile, name="profile"),
+    path('<int:user_id>/followers/', followers, name="followers"),
+    path('<int:user_id>/following/', following, name="following"),
+    path("<int:user_id>/follow/", follow, name="follow"),
 ]
